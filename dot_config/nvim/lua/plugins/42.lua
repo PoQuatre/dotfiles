f62d5f43f352c2182d8f95e496282524cf58a5c6
PoQuatre <leflem.mateo2002@gmail.com>
2024-09-08 04:35:24 +0200

    feat: add some nvim configs for 42

---
+++ dot_config/nvim/lua/plugins/42.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "Diogo-ss/42-header.nvim",
+    cmd = { "Stdheader" },
+    keys = { "<F1>" },
+    opts = {
+      user = "mle-flem",
+      mail = "mle-flem@student.42.fr",
+    },
+  },
+}
