3fc2954923c48aaa2ee31883fc913b3342ea5767
PoQuatre <leflem.mateo2002@gmail.com>
2024-10-21 19:50:58 +0200

    chore: always complete on <c-space> in insert mode

--- dot_config/nvim/lua/plugins/supertab.lua
+++ dot_config/nvim/lua/plugins/supertab.lua
@@ -4,22 +4,12 @@ return {
 
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
-    local has_words_before = function()
-      unpack = unpack or table.unpack
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-    end
-
     local cmp = require("cmp")
 
     table.insert(opts.sources, { name = "emoji" })
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<c-Space>"] = cmp.mapping(function(fallback)
-        if has_words_before() then
-          cmp.complete()
-        else
-          fallback()
-        end
+      ["<c-Space>"] = cmp.mapping(function()
+        cmp.complete()
       end, { "i" }),
       ["<Tab>"] = cmp.mapping(function(fallback)
         if cmp.visible() then
