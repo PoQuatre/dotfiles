98eeaa6edc73a41c61594266835651361323d881
PoQuatre <leflem.mateo2002@gmail.com>
2024-10-21 19:48:59 +0200

    fix: add support to .sqlfluff config file

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,13 +1,38 @@
 return {
-  "nvim-lspconfig",
+  {
+    "nvim-lspconfig",
 
-  opts = function(_, opts)
-    opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
-      emmet_language_server = {},
-      templ = {},
-      htmx = {},
-    })
+    opts = function(_, opts)
+      opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
+        emmet_language_server = {
+          filetypes = vim.list_extend(
+            { "templ" },
+            require("lspconfig/configs/emmet_language_server").default_config.filetypes
+          ),
+        },
+        templ = {},
+        htmx = {},
+      })
 
-    return opts
-  end,
+      return opts
+    end,
+  },
+
+  {
+    "nvim-lint",
+    opts = function(_, opts)
+      opts.linters.sqlfluff = {
+        args = { "lint", "--format=json" },
+      }
+    end,
+  },
+
+  {
+    "conform.nvim",
+    opts = function(_, opts)
+      opts.formatters.sqlfluff = {
+        args = { "format", "-" },
+      }
+    end,
+  },
 }
