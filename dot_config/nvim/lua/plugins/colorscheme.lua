cd7b7d37bb2d30f4497ed7522217d48ec0ca89ab
PoQuatre <leflem.mateo2002@gmail.com>
2024-11-03 20:42:11 +0100

    fix: change lualine spacing

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -14,4 +14,39 @@ return {
       return opts
     end,
   },
+
+  {
+    "lualine.nvim",
+    opts = function(_, opts)
+      require("lazy.status").updates()
+      opts.options = vim.tbl_deep_extend("force", opts.options or {}, {
+        -- theme = "catppuccin",
+        component_separators = "|",
+        section_separators = "",
+        globalstatus = true,
+      })
+
+      local icons = LazyVim.config.icons
+      opts.sections = vim.tbl_deep_extend("force", opts.sections or {}, {
+        lualine_c = {
+          LazyVim.lualine.root_dir({ icon = "󱉭" }),
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          { LazyVim.lualine.pretty_path(), padding = { left = 0 } },
+        },
+      })
+
+      opts.tabline = {}
+
+      return opts
+    end,
+  },
 }
