ad70517e95c79619ee594275a453638c4903e27b
PoQuatre <leflem.mateo2002@gmail.com>
2024-11-03 20:41:40 +0100

    fix: only enable golangci-lint when it's config file is found

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -3,11 +3,22 @@ return {
     "nvim-lspconfig",
 
     opts = function(_, opts)
+      local util = require("lspconfig.util")
+
       opts.servers = vim.tbl_deep_extend("force", opts.servers or {}, {
         emmet_language_server = {
           filetypes = vim.list_extend(
             { "templ" },
             require("lspconfig/configs/emmet_language_server").default_config.filetypes
+          ),
+        },
+        golangci_lint_ls = {
+          -- stylua: ignore
+          root_dir = util.root_pattern(
+            ".golangci.yml",
+            ".golangci.yaml",
+            ".golangci.toml",
+            ".golangci.json"
           ),
         },
         templ = {},
