9f4895f2dfe2720fd1ebc740acd2e566c504952c
mle-flem <mle-flem@student.42.fr>
2024-11-22 17:58:03 +0100

    feat: add the ZenMode keybind

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,26 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.g.zenmode = false
+
+LazyVim.toggle.map(
+  "<leader>uz",
+  LazyVim.toggle.wrap({
+    name = "Zen mode",
+    get = function()
+      return vim.g.zenmode
+    end,
+    set = function(state)
+      vim.cmd("Gitsigns toggle_signs " .. tostring(not state))
+      LazyVim.toggle.diagnostics.set(not state)
+      LazyVim.toggle.inlay_hints.set(not state)
+      if state then
+        vim.cmd("ScrollbarHide")
+      else
+        vim.cmd("ScrollbarShow")
+      end
+      vim.g.zenmode = not vim.g.zenmode
+    end,
+  })
+)
